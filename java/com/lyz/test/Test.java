05359fe78c8ea96c3c5ec1d282b167c545e79d1c
luyuzhi <lyz100417@163.com>
2017-06-13 15:54:33 +0800

    init

---
+++ java/com/lyz/test/Test.java
@@ -0,0 +1,14 @@
+package com.lyz.test;
+
+/**
+ * Author: Luyuzhi
+ * Date: 2017/6/13
+ * E-Mail: luyuzhi@kokozu.net
+ */
+
+public class Test {
+
+    public static String getDesc(){
+        return "is subsecond";
+    }
+}
